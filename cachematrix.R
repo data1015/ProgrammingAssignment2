d11afb243a25142f1fdc2aa8d63390b0ac4dfb50
data1015 <data.github@excel4x.com>
2015-08-22 22:58:49 -0700

    2nd commit - added more commments.

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,7 @@
 
 ## Write a short comment describing this function
 ## This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix must be called before cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -20,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
